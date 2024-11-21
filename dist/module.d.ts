4f4b282c345afcb9fcb7de5f8dce0ccb73bccac4
Mihai Oprea <mishu@mishulica.com>
2024-11-21 12:47:41 +0200

    commited dist

---
+++ dist/module.d.ts
@@ -0,0 +1,2 @@
+export * from "/Users/mihai/PycharmProjects/nuxt-api-party/src/module";
+export { default } from "/Users/mihai/PycharmProjects/nuxt-api-party/src/module";
---
+++ dist/module.mjs
@@ -0,0 +1,12 @@
+import jiti from "file:///Users/mihai/PycharmProjects/nuxt-api-party/node_modules/.pnpm/jiti@1.21.6/node_modules/jiti/lib/index.js";
+
+/** @type {import("/Users/mihai/PycharmProjects/nuxt-api-party/src/module")} */
+const _module = jiti(null, {
+  "esmResolve": true,
+  "interopDefault": true,
+  "alias": {
+    "nuxt-api-party": "/Users/mihai/PycharmProjects/nuxt-api-party"
+  }
+})("/Users/mihai/PycharmProjects/nuxt-api-party/src/module.ts");
+
+export default _module;
